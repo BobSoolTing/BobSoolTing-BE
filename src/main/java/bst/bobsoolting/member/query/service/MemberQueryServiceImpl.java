b5e20a8025128b740cbbad86e31cbceee875ac19
JoJeHuni <cho9759@gmail.com>
2025-02-25 00:45:27 +0900

    fix: implements 추가

--- src/main/java/bst/bobsoolting/member/query/service/MemberQueryServiceImpl.java
+++ src/main/java/bst/bobsoolting/member/query/service/MemberQueryServiceImpl.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class MemberQueryServiceImpl {
+public class MemberQueryServiceImpl implements MemberQueryService {
 }
