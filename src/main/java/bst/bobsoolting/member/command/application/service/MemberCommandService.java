215171714341be10bc1f8c51a1d5322df4028b00
JoJeHuni <cho9759@gmail.com>
2025-02-28 02:29:17 +0900

    feat: 신규 유저 등록 시 memberId 지정 후 저장하는 서비스 코드 추가

--- src/main/java/bst/bobsoolting/member/command/application/service/MemberCommandService.java
+++ src/main/java/bst/bobsoolting/member/command/application/service/MemberCommandService.java
@@ -1,4 +1,7 @@
 package bst.bobsoolting.member.command.application.service;
 
+import bst.bobsoolting.member.command.application.dto.MemberDTO;
+
 public interface MemberCommandService {
+    void createMember(MemberDTO newMember);
 }
