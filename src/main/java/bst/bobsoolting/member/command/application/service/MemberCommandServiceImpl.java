43554fc7708cf994568eb2fa5df11fe762820963
JoJeHuni <cho9759@gmail.com>
2025-02-28 18:56:02 +0900

    feat: 프로필 수정 서비스 코드 추가

--- src/main/java/bst/bobsoolting/member/command/application/service/MemberCommandServiceImpl.java
+++ src/main/java/bst/bobsoolting/member/command/application/service/MemberCommandServiceImpl.java
@@ -6,9 +6,13 @@ import bst.bobsoolting.member.command.application.dto.MemberDTO;
 import bst.bobsoolting.member.command.application.mapper.MemberConverter;
 import bst.bobsoolting.member.command.domain.aggregate.entity.Member;
 import bst.bobsoolting.member.command.domain.repository.MemberRepository;
+import bst.bobsoolting.member.command.domain.vo.request.RequestUpdateProfileVO;
+import bst.bobsoolting.member.query.repository.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -20,6 +24,7 @@ import java.util.UUID;
 public class MemberCommandServiceImpl implements MemberCommandService {
 
     private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
     private final MemberConverter memberConverter;
 
     @Override
@@ -39,10 +44,44 @@ public class MemberCommandServiceImpl implements MemberCommandService {
         }
     }
 
+    @Override
+    @Transactional
+    public MemberDTO updateMemberProfile(OAuth2User user, RequestUpdateProfileVO updateInfo) {
+        Long kakaoIdLong = user.getAttribute("id");
+        String kakaoId = String.valueOf(kakaoIdLong);
+        log.info("프로필 수정 진행. kakaoId={}, 변경 정보={}", kakaoId, updateInfo);
+
+        Member member = memberMapper.findByKakaoId(kakaoId);
+        if (member == null) throw new CommonException(ErrorCode.NOT_FOUND_MEMBER);
+
+        boolean isUpdated = false;
+        if (updateInfo.getProfileImage() != null) {
+            member.setProfileImage(updateInfo.getProfileImage());
+            isUpdated = true;
+        }
+        if (updateInfo.getNickname() != null) {
+            member.setNickname(updateInfo.getNickname());
+            isUpdated = true;
+        }
+        if (updateInfo.getDepartment() != null) {
+            member.setDepartment(updateInfo.getDepartment());
+            isUpdated = true;
+        }
+
+        if (!isUpdated) {
+            log.info("변경할 항목이 없음. kakaoId={}", kakaoId);
+            return memberConverter.fromEntityToDTO(member);
+        }
+
+        memberRepository.save(member);
+        log.info("프로필 수정 완료. kakaoId={}, 수정된 정보={}", kakaoId, updateInfo);
+
+        return memberConverter.fromEntityToDTO(member);
+    }
+
     private String generateMemberId() {
         String datePart = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         String uuidPart = UUID.randomUUID().toString().replace("-", "").substring(20);
-
         return datePart + uuidPart;
     }
 }
