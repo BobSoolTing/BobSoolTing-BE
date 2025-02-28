d37e669c44330c9bbe3a8ae2e626fa508455e1e0
JoJeHuni <cho9759@gmail.com>
2025-03-01 00:35:26 +0900

    fix: MemberController 에 description 추가

--- src/main/java/bst/bobsoolting/member/command/application/controller/MemberCommandController.java
+++ src/main/java/bst/bobsoolting/member/command/application/controller/MemberCommandController.java
@@ -7,6 +7,7 @@ import bst.bobsoolting.member.command.domain.vo.request.RequestAdditionalRegiste
 import bst.bobsoolting.member.command.domain.vo.request.RequestUpdateProfileVO;
 import bst.bobsoolting.member.command.domain.vo.response.ResponseCreateMemberVO;
 import bst.bobsoolting.member.command.domain.vo.response.ResponseProfileVO;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -24,6 +25,7 @@ public class MemberCommandController {
     private final MemberCommandService memberCommandService;
     private final MemberConverter memberConverter;
 
+    @Operation(description = "추가 회원가입 정보 등록")
     @PostMapping("/complete-registration")
     public ResponseEntity<?> completeRegistration(@RequestBody RequestAdditionalRegisterVO info) {
         log.info("추가 회원가입 정보 수신: {}", info);
@@ -33,6 +35,7 @@ public class MemberCommandController {
         return ResponseEntity.status(HttpStatus.OK).body(memberVO);
     }
 
+    @Operation(description = "회원 정보 수정")
     @PatchMapping("/update-profile")
     public ResponseEntity<?> updateProfile(@AuthenticationPrincipal OAuth2User user, @RequestBody RequestUpdateProfileVO updateInfo) {
         log.info("프로필 수정 요청: {} -> {}", user.getAttribute("id"), updateInfo);
