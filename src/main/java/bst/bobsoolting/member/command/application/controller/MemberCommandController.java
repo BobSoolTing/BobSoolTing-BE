103858aed0dde6442d83b9ef05d75ee119029866
JoJeHuni <cho9759@gmail.com>
2025-02-28 18:56:09 +0900

    feat: 프로필 수정 api 추가

--- src/main/java/bst/bobsoolting/member/command/application/controller/MemberCommandController.java
+++ src/main/java/bst/bobsoolting/member/command/application/controller/MemberCommandController.java
@@ -4,15 +4,16 @@ import bst.bobsoolting.member.command.application.dto.MemberDTO;
 import bst.bobsoolting.member.command.application.mapper.MemberConverter;
 import bst.bobsoolting.member.command.application.service.MemberCommandService;
 import bst.bobsoolting.member.command.domain.vo.request.RequestAdditionalRegisterVO;
+import bst.bobsoolting.member.command.domain.vo.request.RequestUpdateProfileVO;
 import bst.bobsoolting.member.command.domain.vo.response.ResponseCreateMemberVO;
+import bst.bobsoolting.member.command.domain.vo.response.ResponseProfileVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/member")
@@ -30,5 +31,13 @@ public class MemberCommandController {
         MemberDTO member = memberCommandService.createMember(newMemberDTO);
         ResponseCreateMemberVO memberVO = memberConverter.fromEntityToCreateVO(member);
         return ResponseEntity.status(HttpStatus.OK).body(memberVO);
+    }
+
+    @PatchMapping("/update-profile")
+    public ResponseEntity<?> updateProfile(@AuthenticationPrincipal OAuth2User user, @RequestBody RequestUpdateProfileVO updateInfo) {
+        log.info("프로필 수정 요청: {} -> {}", user.getAttribute("id"), updateInfo);
+        MemberDTO updatedMember = memberCommandService.updateMemberProfile(user, updateInfo);
+        ResponseProfileVO responseProfileVO = memberConverter.fromEntityToProfileVO(updatedMember);
+        return ResponseEntity.ok(responseProfileVO);
     }
 }
