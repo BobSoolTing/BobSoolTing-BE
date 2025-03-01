3e0e02d59fe3c814edac77ff63c307dec8b74bcb
JoJeHuni <cho9759@gmail.com>
2025-03-01 17:44:27 +0900

    feat: 댓글 조회 시 대댓글도 함께 조회하는 쿼리문 추가

--- src/main/java/bst/bobsoolting/comment/query/repository/CommentMapper.java
+++ src/main/java/bst/bobsoolting/comment/query/repository/CommentMapper.java
@@ -1,6 +1,7 @@
 package bst.bobsoolting.comment.query.repository;
 
 import bst.bobsoolting.comment.command.domain.aggregate.entity.Comment;
+import bst.bobsoolting.reply.command.domain.aggregate.entity.Reply;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,4 +10,6 @@ import java.util.List;
 @Mapper
 public interface CommentMapper {
     List<Comment> findCommentsByPost(@Param("postId") Long postId, @Param("cursor") Long cursor);
+
+    List<Reply> findRepliesByCommentId(@Param("commentId") Long commentId);
 }
