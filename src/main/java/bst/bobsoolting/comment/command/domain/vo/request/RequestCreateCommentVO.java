65a0e03a9bc9727135c6732462ac2ae78968e7c3
JoJeHuni <cho9759@gmail.com>
2025-03-04 21:22:28 +0900

    hotfix: swagger 확인을 위해 Schema 추가

--- src/main/java/bst/bobsoolting/comment/command/domain/vo/request/RequestCreateCommentVO.java
+++ src/main/java/bst/bobsoolting/comment/command/domain/vo/request/RequestCreateCommentVO.java
@@ -1,6 +1,7 @@
 package bst.bobsoolting.comment.command.domain.vo.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,7 +9,10 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 @ToString
+@Schema(description = "댓글 생성 요청 객체")
 public class RequestCreateCommentVO {
+
+    @Schema(description = "댓글 내용", example = "이거 정말 좋은 글이네요!")
     @JsonProperty("comment_content")
     private String commentContent;
 }
--- src/main/java/bst/bobsoolting/reply/command/domain/vo/response/ResponseReplyVO.java
+++ src/main/java/bst/bobsoolting/reply/command/domain/vo/response/ResponseReplyVO.java
@@ -1,6 +1,7 @@
 package bst.bobsoolting.reply.command.domain.vo.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,16 +11,22 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @ToString
+@Schema(description = "대댓글 응답 객체")
 public class ResponseReplyVO {
+
+    @Schema(description = "대댓글 ID", example = "2001")
     @JsonProperty("reply_id")
     private Long replyId;
 
+    @Schema(description = "대댓글 내용", example = "저도 그렇게 생각해요!")
     @JsonProperty("reply_content")
     private String replyContent;
 
+    @Schema(description = "작성자 ID", example = "user5678")
     @JsonProperty("member_id")
     private String memberId;
 
+    @Schema(description = "대댓글 작성 시간", example = "2024-03-04T14:30:00")
     @JsonProperty("created_at")
     private LocalDateTime createdAt;
 }
