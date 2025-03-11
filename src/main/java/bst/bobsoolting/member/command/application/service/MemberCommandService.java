7eb753e93bf49be81c64919317382666a3532080
JoJeHuni <cho9759@gmail.com>
2025-03-11 21:37:46 +0900

    fix: 초기 멤버 값 세팅 수정

--- src/main/java/bst/bobsoolting/member/command/application/service/MemberCommandService.java
+++ src/main/java/bst/bobsoolting/member/command/application/service/MemberCommandService.java
@@ -15,7 +15,7 @@ public interface MemberCommandService {
     MemberDTO getKakaoUserInfo(String accessToken);
 
     @Transactional
-    MemberDTO createOrUpdateMember(String kakaoId, String nickname);
+    MemberDTO createOrUpdateMember(String kakaoId);
 
     MemberDTO updateMemberAdditionalInfo(String kakaoId, RequestAdditionalRegisterVO info);
 
