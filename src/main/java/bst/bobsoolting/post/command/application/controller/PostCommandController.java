06697d6da9bee4015f19e47833aca4cbdf06e046
JoJeHuni <cho9759@gmail.com>
2025-03-16 13:43:40 +0900

    feat: post 응답 필드 추가

--- src/main/java/bst/bobsoolting/post/command/application/controller/PostCommandController.java
+++ src/main/java/bst/bobsoolting/post/command/application/controller/PostCommandController.java
@@ -3,9 +3,12 @@ package bst.bobsoolting.post.command.application.controller;
 import bst.bobsoolting.member.query.service.MemberQueryService;
 import bst.bobsoolting.post.command.application.controller.docs.PostCommandControllerDocs;
 import bst.bobsoolting.post.command.application.dto.PostDTO;
+import bst.bobsoolting.post.command.application.mapper.PostConverter;
 import bst.bobsoolting.post.command.application.service.PostCommandService;
 import bst.bobsoolting.post.command.domain.vo.request.RequestCreatePostVO;
 import bst.bobsoolting.post.command.domain.vo.request.RequestUpdatePostVO;
+import bst.bobsoolting.post.command.domain.vo.response.ResponseCreatePostVO;
+import bst.bobsoolting.post.command.domain.vo.response.ResponseUpdatePostVO;
 import bst.bobsoolting.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,27 +22,30 @@ import org.springframework.http.HttpHeaders;
 public class PostCommandController implements PostCommandControllerDocs {
 
     private final PostCommandService postCommandService;
+    private final PostConverter postConverter;
     private final MemberQueryService memberQueryService;
     private final SecurityUtil securityUtil;
 
     @PostMapping
-    public ResponseEntity<PostDTO> createPost(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody RequestCreatePostVO request) {
+    public ResponseEntity<ResponseCreatePostVO> createPost(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody RequestCreatePostVO request) {
         String kakaoId = securityUtil.getKakaoIdFromToken(token.replace("Bearer ", ""));
         String memberId = memberQueryService.getMemberIdByKakaoId(kakaoId);
         if (memberId == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
 
         PostDTO created = postCommandService.createPost(request, memberId);
-        return ResponseEntity.ok(created);
+        ResponseCreatePostVO response = postConverter.fromDTOToCreateVO(created);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{postId}")
-    public ResponseEntity<PostDTO> updatePost(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @PathVariable Long postId, @RequestBody RequestUpdatePostVO request) {
+    public ResponseEntity<ResponseUpdatePostVO> updatePost(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @PathVariable Long postId, @RequestBody RequestUpdatePostVO request) {
         String kakaoId = securityUtil.getKakaoIdFromToken(token.replace("Bearer ", ""));
         String memberId = memberQueryService.getMemberIdByKakaoId(kakaoId);
         if (memberId == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
 
         PostDTO updated = postCommandService.updatePost(memberId, postId, request);
-        return ResponseEntity.ok(updated);
+        ResponseUpdatePostVO response = postConverter.fromDTOToUpdateVO(updated);
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/{postId}/status")
---
+++ src/main/java/bst/bobsoolting/post/command/domain/vo/response/ResponseUpdatePostVO.java
@@ -0,0 +1,43 @@
+package bst.bobsoolting.post.command.domain.vo.response;
+
+import bst.bobsoolting.post.command.domain.aggregate.Category;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Schema(name = "ResponseUpdatePostVO", description = "게시글 등록 정보")
+public class ResponseUpdatePostVO {
+    @Schema(description = "게시글 ID", example = "1L")
+    @JsonProperty("post_id")
+    private Long postId;
+
+    @Schema(description = "카테고리 (FOOD, DRINK, MEETING)", example = "FOOD")
+    private Category category;
+
+    @Schema(description = "제목", example = "저녁 드실 분! (수정)")
+    private String title;
+
+    @Schema(description = "내용", example = "3월 16일에 저녁 드실 분 구합니다.")
+    private String content;
+
+    @Schema(description = "최대 참가자 수", example = "7")
+    @JsonProperty("max_participants")
+    private Integer maxParticipants;
+
+    @Schema(description = "활동 날짜", example = "2025-03-16")
+    private LocalDate date;
+
+    @Schema(description = "장소", example = "서울 강서구")
+    private String location;
+
+    @Schema(description = "작성자 ID", example = "연월일 8글자 + UUID")
+    @JsonProperty("member_id")
+    private String memberId;
+}
